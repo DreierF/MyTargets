a9edee7a984464c2def42b2217256a292d31298a
Florian Dreier <dreier.florian@gmail.com>
2015-11-22 17:37:39 +0100

    Modified to provide similar results as before

--- app/src/main/java/de/dreier/mytargets/fragments/StatisticsFragment.java
+++ app/src/main/java/de/dreier/mytargets/fragments/StatisticsFragment.java
@@ -9,18 +9,13 @@ package de.dreier.mytargets.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.SortedSet;
 
 import de.dreier.mytargets.R;
@@ -32,8 +27,6 @@ public class StatisticsFragment extends Fragment {
     public static final String ARG_POSITION = "position";
     public static final String ARG_TRAINING_ID = "training_id";
     public static final String ARG_ROUND_ID = "round_id";
-    private long mTraining;
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -41,66 +34,39 @@ public class StatisticsFragment extends Fragment {
                 .inflate(R.layout.fragment_statistics, container, false);
 
         int pos = getArguments().getInt(ARG_POSITION, 0);
-        mTraining = getArguments().getLong(ARG_TRAINING_ID, 0);
+        long training = getArguments().getLong(ARG_TRAINING_ID, 0);
 
         LineChart chartView = (LineChart) rootView.findViewById(R.id.chart_view);
 
-        LineDataSet series;
+        LineData data;
 
         switch (pos) {
             case 0:
-                series = generateAllSeries();
+                data = new StatisticsDataSource(getContext()).getAllTrainings();
                 break;
             default:
-                series = generateTrainingSeries();
+                data = new StatisticsDataSource(getContext()).getAllRounds(training);
                 break;
             //default:
             //    series = generateRoundSeries();
         }
-        LineData data = new LineData();
-        DisplayMetrics metrics = getResources().getDisplayMetrics();
         /*LineDataSet regression = generateLinearRegression(series);
         if (regression != null) {
             regression.setLineColor(0xFFAA66CC);
-            regression.setLineWidth(1.3f * metrics.density);
+            regression.setLineWidth(1.3);
             data.addDataSet(regression);
         }*/
 
-        series.setColors(new int[]{0xFF33B5E5});
-        series.setLineWidth(2 * metrics.density);
-        data.addDataSet(series);
+        chartView.getXAxis().setEnabled(false);
+        chartView.getAxisRight().setEnabled(false);
         chartView.setData(data);
+        chartView.getLegend().setEnabled(false);
+        chartView.setDescription("");
+        chartView.animateXY(2000, 2000);
 
         return rootView;
     }
 
-    private LineDataSet generateAllSeries() {
-        List<Entry> list = new StatisticsDataSource(getContext()).getAllTrainings();
-        return new LineDataSet(list, "All trainings");
-    }
-
-    private LineDataSet generateTrainingSeries() {
-        ArrayList<Entry> list = new StatisticsDataSource(getContext()).getAllRounds(mTraining);
-        return new LineDataSet(list, "Training");
-    }
-
-    /*private LineDataSet generateRoundSeries() {
-        PasseDataSource passeDataSource = new PasseDataSource(getContext());
-        ArrayList<Passe> passes = passeDataSource.getAll(mRound);
-        RoundDataSource roundDataSource = new RoundDataSource(getContext());
-        Round r = roundDataSource.get(mRound);
-
-        LinearSeries series = new LinearSeries();
-
-        int x = 0;
-        for (Passe passe : passes) {
-            for (Shot shot : passe.shot) {
-                series.addPoint(new LinearPoint(x++, (long) shot.zone));
-            }
-        }
-        //chartView.setRoundInfo(r);
-        return series;
-    }*/
 
     private LinearSeries generateLinearRegression(LinearSeries data) {
         SortedSet<LinearPoint> points = data.getPoints();
--- app/src/main/java/de/dreier/mytargets/managers/dao/StatisticsDataSource.java
+++ app/src/main/java/de/dreier/mytargets/managers/dao/StatisticsDataSource.java
@@ -8,11 +8,13 @@ package de.dreier.mytargets.managers.dao;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.support.annotation.NonNull;
 
 import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import de.dreier.mytargets.shared.models.target.Target;
 import de.dreier.mytargets.shared.models.target.TargetFactory;
@@ -23,13 +25,19 @@ public class StatisticsDataSource extends DataSourceBase {
         super(context);
     }
 
-    public List<Entry> getAllTrainings() {
+    public LineData getAllTrainings() {
         Cursor res = database
                 .rawQuery("SELECT s.points, r.target, s.passe, r.scoring_style, s.arrow_index  " +
                         "FROM TRAINING t, ROUND r, PASSE p, SHOOT s " +
                         "WHERE t._id=r.training AND r._id=p.round " +
                         "AND p._id=s.passe " +
                         "ORDER BY t.datum, t._id, r._id, p._id, s._id", null);
+
+        return getLineDataFromPasseCursor(res);
+    }
+
+    @NonNull
+    private LineData getLineDataFromPasseCursor(Cursor res) {
         res.moveToFirst();
 
         int oldPasse = -1;
@@ -54,41 +62,29 @@ public class StatisticsDataSource extends DataSourceBase {
         float percent = actCounter * 100.0f / (float) maxCounter;
         history.add(new Entry(percent, passeIndex));
         res.close();
-        return history;
+
+
+        LineDataSet series = new LineDataSet(history, "All trainings");
+        series.setColors(new int[]{0xFF33B5E5});
+        series.setLineWidth(2);
+
+        ArrayList<String> xValues = new ArrayList<>();
+        for (int i = 0; i < series.getEntryCount(); i++) {
+            xValues.add(String.valueOf(i));
+        }
+        LineData data = new LineData(xValues, series);
+        data.setDrawValues(false);
+        return data;
     }
 
-    public ArrayList<Entry> getAllRounds(long training) {
+    public LineData getAllRounds(long training) {
         Cursor res = database
                 .rawQuery("SELECT s.points, r.target, s.passe, r.scoring_style, s.arrow_index " +
                         "FROM ROUND r, PASSE p, SHOOT s " +
                         "WHERE " + training + "=r.training AND r._id=p.round " +
                         "AND p._id=s.passe " +
                         "ORDER BY r._id, p._id, s._id", null);
-        res.moveToFirst();
-
-        int oldPasse = -1;
-        int actCounter = 0, maxCounter = 0;
-        int passeIndex = 0;
-        ArrayList<Entry> history = new ArrayList<>();
-        for (int i = 0; i < res.getCount(); i++) {
-            int zone = res.getInt(0);
-            int passe = res.getInt(2);
-            if (oldPasse != -1 && oldPasse != passe) {
-                float percent = actCounter * 100.0f / (float) maxCounter;
-                history.add(new Entry(percent, passeIndex++));
-                actCounter = 0;
-                maxCounter = 0;
-            }
-            Target target = TargetFactory.createTarget(context, res.getInt(1), res.getInt(3));
-            actCounter += target.getPointsByZone(zone, res.getInt(4));
-            maxCounter += target.getMaxPoints();
-            oldPasse = passe;
-            res.moveToNext();
-        }
-        float percent = actCounter * 100.0f / (float) maxCounter;
-        history.add(new Entry(percent, passeIndex));
-        res.close();
-        return history;
+        return getLineDataFromPasseCursor(res);
     }
 
 }
