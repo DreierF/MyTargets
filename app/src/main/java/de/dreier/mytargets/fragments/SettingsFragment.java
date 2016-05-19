840da00b462a452f23e439050e55489cc31e5596
Florian Dreier <dreier.florian@gmail.com>
2016-05-19 17:53:09 +0200

    Removed duplicate method

--- app/src/main/java/de/dreier/mytargets/fragments/SettingsFragment.java
+++ app/src/main/java/de/dreier/mytargets/fragments/SettingsFragment.java
@@ -212,17 +212,4 @@ public class SettingsFragment extends PreferenceFragmentCompat
         }
         pref.setSummary(getResources().getQuantityString(R.plurals.second, sec, sec));
     }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 1 && resultCode == AppCompatActivity.RESULT_OK) {
-            final Uri uri = data.getData();
-            if (BackupUtils.Import(getActivity(), uri)) {
-                Intent intent = new Intent(getContext(), MainActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-            }
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
 }
