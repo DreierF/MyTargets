01cbb2c25b65e50c42793bc2d70821339931832e
Florian Dreier <dreier.florian@gmail.com>
2016-05-29 17:11:32 +0200

    Added launch screen

--- app/src/main/java/de/dreier/mytargets/activities/MainActivity.java
+++ app/src/main/java/de/dreier/mytargets/activities/MainActivity.java
@@ -85,6 +85,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_CustomToolbar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
