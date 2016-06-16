b35fccb675e3e8bdc47e6399a05e8e78c08cfaf5
Florian Dreier <dreier.florian@gmail.com>
2016-06-16 13:29:40 +0200

    Closed #75 Updated translations

--- app/src/main/java/de/dreier/mytargets/fragments/DatePickerFragment.java
+++ app/src/main/java/de/dreier/mytargets/fragments/DatePickerFragment.java
@@ -6,27 +6,20 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import org.joda.time.LocalDate;
 
 public class DatePickerFragment extends DialogFragment {
 
-    public static final String ARG_CURRENT_DATE = "current_date";
+    static final String ARG_CURRENT_DATE = "current_date";
 
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the current date as the default date in the picker
-        Date date = (Date) getArguments().getSerializable(ARG_CURRENT_DATE);
-        Calendar calendar = new GregorianCalendar();
-        calendar.setTimeInMillis(date.getTime());
-        int year = calendar.get(Calendar.YEAR);
-        int month = calendar.get(Calendar.MONTH);
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        LocalDate date = (LocalDate) getArguments().getSerializable(ARG_CURRENT_DATE);
 
         // Create a new instance of DatePickerDialog and return it
         DatePickerDialog.OnDateSetListener listener = (DatePickerDialog.OnDateSetListener) getTargetFragment();
-        return new DatePickerDialog(getActivity(), listener, year, month, day);
+        return new DatePickerDialog(getActivity(), listener, date.getYear(), date.getMonthOfYear()-1, date.getDayOfMonth());
     }
 }
