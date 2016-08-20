15113ff5037e86689d9b9988ec7d3c054603f9a4
Florian Dreier <dreier.florian@gmail.com>
2016-08-20 13:55:27 +0200

    Fixed #134

--- app/src/main/java/de/dreier/mytargets/fragments/EditTrainingFragment.java
+++ app/src/main/java/de/dreier/mytargets/fragments/EditTrainingFragment.java
@@ -159,7 +159,6 @@ public class EditTrainingFragment extends EditFragmentBase implements DatePicker
             binding.numberArrows.setVisibility(View.GONE);
         } else {
             binding.numberArrows.setVisibility(View.VISIBLE);
-            binding.numberArrows.setChecked(true);
         }
     }
 
