b92671f7ed00e6d609d811625aa3eeef179945e3
Florian Dreier <dreier.florian@gmail.com>
2016-10-17 08:19:25 +0200

    Fixed 6 spot Added 3 Spot

--- shared/src/main/java/de/dreier/mytargets/shared/targets/WADanage6Spot.java
+++ shared/src/main/java/de/dreier/mytargets/shared/targets/WADanage6Spot.java
@@ -13,18 +13,18 @@ import de.dreier.mytargets.shared.models.Dimension;
 import static de.dreier.mytargets.shared.models.Dimension.Unit.CENTIMETER;
 
 public class WADanage6Spot extends WA3Ring {
-    public static final int ID = 26;
+    public static final int ID = 27;
 
     public WADanage6Spot() {
         super(ID, R.string.wa_danage_6_spot);
-        faceRadius = 190;
+        faceRadius = 193;
         facePositions = new Coordinate[]{
-                new Coordinate(500, 500),
-                new Coordinate(800, 430),
-                new Coordinate(690, 790),
-                new Coordinate(310, 790),
-                new Coordinate(190, 430),
-                new Coordinate(500, 210)
+                new Coordinate(500, 515.7644f),
+                new Coordinate(500, 193.46f),
+                new Coordinate(806.54f, 416.16437f),
+                new Coordinate(193.46f, 416.16437f),
+                new Coordinate(689.461f, 776.5344f),
+                new Coordinate(310.539f, 776.5344f)
         };
         diameters = new Dimension[]{
                 new Dimension(40, CENTIMETER),
