583e9c7ae27e42eee999a78b50a874d097c6f39e
Florian Dreier <dreier.florian@gmail.com>
2017-03-19 16:15:57 +0100

    Removed unused imports

--- app/src/main/java/de/dreier/mytargets/features/settings/backup/provider/ExternalStorageBackup.java
+++ app/src/main/java/de/dreier/mytargets/features/settings/backup/provider/ExternalStorageBackup.java
@@ -17,7 +17,6 @@ package de.dreier.mytargets.features.settings.backup.provider;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
