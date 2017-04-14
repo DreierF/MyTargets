2fad15c09bdfdf8b807e4d0a996996cf364dd5ed
Florian Dreier <dreier.florian@gmail.com>
2017-04-14 15:44:49 +0200

    Disabled StandardRoundActivityTest

--- app/src/androidTest/java/de/dreier/mytargets/features/training/standardround/StandardRoundActivityTest.java
+++ app/src/androidTest/java/de/dreier/mytargets/features/training/standardround/StandardRoundActivityTest.java
@@ -19,6 +19,7 @@ import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -42,6 +43,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static de.dreier.mytargets.test.utils.matchers.RecyclerViewMatcher.withRecyclerView;
 
+@Ignore
 @RunWith(AndroidJUnit4.class)
 public class StandardRoundActivityTest extends UITestBase {
 
