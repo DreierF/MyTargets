0c09465286500c8dbf3cc4f1d76b83490627a138
Florian Dreier <dreier.florian@gmail.com>
2017-12-15 17:45:00 +0100

    Converted EndRenderer to Kotlin

--- shared/src/main/java/de/dreier/mytargets/shared/utils/EndRenderer.kt
+++ shared/src/main/java/de/dreier/mytargets/shared/utils/EndRenderer.kt
@@ -13,241 +13,236 @@
  * GNU General Public License for more details.
  */
 
-package de.dreier.mytargets.shared.utils;
+package de.dreier.mytargets.shared.utils
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.ValueAnimator;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.PointF;
-import android.graphics.Rect;
-import android.graphics.RectF;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ValueAnimator
+import android.annotation.SuppressLint
+import android.graphics.*
+import android.os.Parcelable
+import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import de.dreier.mytargets.shared.models.Target
+import de.dreier.mytargets.shared.models.db.Shot
+import kotlinx.android.parcel.Parcelize
 
-import org.parceler.Parcel;
+@SuppressLint("ParcelCreator")
+@Parcelize
+class EndRenderer(
+        private var shotList: List<Shot>? = null,
+        private var pressed: Int = NO_SELECTION,
+        internal var selected: Int = NO_SELECTION,
+        private var selectedPosition: PointF? = null,
+        private var selectedRadius: Int = 0,
+        private var oldCoordinate: Array<PointF?>,
+        internal var currentAnimationProgress: Float = -1f,
+        private var ambientMode: Boolean = false
+) : Parcelable {
 
-import java.util.List;
+    @Transient private var circle: Circle? = null
+    @Transient private var parent: View? = null
+    @Transient private var rect: RectF? = null
+    @Transient private var radius: Int = 0
+    @Transient private var grayBackground = Paint()
+    @Transient private var density: Float = 0.toFloat()
+    @Transient private var shotsPerRow: Int = 0
+    @Transient private var rowHeight: Float = 0.toFloat()
+    @Transient private var columnWidth: Float = 0.toFloat()
+    @Transient private var oldRadius: Int = 0
+    @Transient private var oldSelected: Int = 0
+    @Transient private var oldSelectedRadius: Int = 0
 
-import de.dreier.mytargets.shared.models.Target;
-import de.dreier.mytargets.shared.models.db.Shot;
+    private val animator: ValueAnimator
+        get() {
+            val selectionAnimator = ValueAnimator.ofFloat(0f, 1f)
+            selectionAnimator.interpolator = AccelerateDecelerateInterpolator()
+            selectionAnimator.addUpdateListener { valueAnimator ->
+                currentAnimationProgress = valueAnimator.animatedValue as Float
+                parent!!.invalidate()
+            }
+            selectionAnimator.addListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    currentAnimationProgress = -1f
+                }
+            })
+            return selectionAnimator
+        }
 
-@Parcel
-public class EndRenderer {
-    public static final int NO_SELECTION = -1;
-    public static final int MAX_CIRCLE_SIZE = 17;
-    private static final int MIN_PADDING = 2;
-
-    List<Shot> shotList;
-    int pressed = NO_SELECTION;
-    int selected = NO_SELECTION;
-    PointF selectedPosition;
-    int selectedRadius;
-    // Animation
-    PointF[] oldCoordinate;
-    float currentAnimationProgress = -1;
-    private transient Circle circle;
-    private transient View parent;
-    private transient RectF rect;
-    private transient int radius;
-    private transient Paint grayBackground;
-    private transient float density;
-    private transient int shotsPerRow;
-    private transient float rowHeight;
-    private transient float columnWidth;
-    private transient int oldRadius;
-    private transient int oldSelected;
-    private transient int oldSelectedRadius;
-    @SuppressWarnings("WeakerAccess")
-    boolean ambientMode = false;
-
-    public void init(View parent, float density, Target target) {
-        this.parent = parent;
-        this.density = density;
-        circle = new Circle(density, target);
-        grayBackground = new Paint();
-        grayBackground.setColor(0xFFDDDDDD);
-        grayBackground.setAntiAlias(true);
+    init {
+        grayBackground.color = -0x222223
+        grayBackground.isAntiAlias = true
     }
 
-    public void setRect(@NonNull RectF rect) {
-        this.rect = rect;
+    fun init(parent: View, density: Float, target: Target) {
+        this.parent = parent
+        this.density = density
+        circle = Circle(density, target)
+    }
+
+    fun setRect(rect: RectF) {
+        this.rect = rect
         if (shotList == null) {
-            return;
+            return
         }
-        radius = MAX_CIRCLE_SIZE + MIN_PADDING;
-        int neededRows;
-        int maxRows;
+        radius = MAX_CIRCLE_SIZE + MIN_PADDING
+        var neededRows: Int
+        var maxRows: Int
         do {
-            neededRows = (int) Math.ceil((radius * 2 * density * shotList.size()) / rect.width());
-            maxRows = (int) Math.floor(rect.height() / (radius * 2 * density));
-            radius--;
-        } while (neededRows > maxRows);
-        radius -= MIN_PADDING;
-        int numRows = Math.max(neededRows, 1);
-        shotsPerRow = (int) Math.ceil(shotList.size() / (float) numRows);
-        rowHeight = rect.height() / numRows;
-        columnWidth = rect.width() / shotsPerRow;
+            neededRows = Math.ceil((radius.toFloat() * 2f * density * shotList!!.size.toFloat() / rect.width()).toDouble()).toInt()
+            maxRows = Math.floor((rect.height() / (radius.toFloat() * 2f * density)).toDouble()).toInt()
+            radius--
+        } while (neededRows > maxRows)
+        radius -= MIN_PADDING
+        val numRows = Math.max(neededRows, 1)
+        shotsPerRow = Math.ceil((shotList!!.size / numRows.toFloat()).toDouble()).toInt()
+        rowHeight = rect.height() / numRows
+        columnWidth = rect.width() / shotsPerRow
     }
 
-    public void setShots(List<Shot> shots) {
-        boolean calcLayout = rect != null && shotList == null;
-        shotList = shots;
-        oldCoordinate = new PointF[shotList.size()];
+    fun setShots(shots: List<Shot>) {
+        val calcLayout = rect != null && shotList == null
+        shotList = shots
+        oldCoordinate = arrayOfNulls(shotList!!.size)
         if (calcLayout) {
-            setRect(rect);
+            setRect(rect!!)
         }
     }
 
-    public void draw(@NonNull Canvas canvas) {
+    fun draw(canvas: Canvas) {
         if (rect == null || shotsPerRow == 0) {
-            return;
+            return
         }
 
         // Draw all points of this end into the given rect
-        for (int i = 0; i < shotList.size(); i++) {
-            Shot shot = shotList.get(i);
-            if (shot.getScoringRing() == Shot.Companion.getNOTHING_SELECTED()) {
-                break;
+        for (i in shotList!!.indices) {
+            val shot = shotList!![i]
+            if (shot.scoringRing == Shot.NOTHING_SELECTED) {
+                break
             }
 
-            int radius = getRadius(shot);
-            PointF coordinate = getAnimatedPosition(i, shot);
+            val radius = getRadius(shot)
+            val coordinate = getAnimatedPosition(i, shot)
             if (radius > 0) {
                 // Draw touch feedback if arrow is pressed
-                if (pressed == shot.getIndex()) {
+                if (pressed == shot.index) {
                     canvas.drawRect(coordinate.x - radius, coordinate.y - radius,
-                            coordinate.x + radius, coordinate.y + radius, grayBackground);
+                            coordinate.x + radius, coordinate.y + radius, grayBackground)
                 }
 
                 // Draw circle
-                circle.draw(canvas, coordinate.x, coordinate.y, shot.getScoringRing(), radius,
-                        shot.getIndex(),
-                        shot.getArrowNumber(), ambientMode);
+                circle!!.draw(canvas, coordinate.x, coordinate.y, shot.scoringRing, radius,
+                        shot.index,
+                        shot.arrowNumber, ambientMode)
             }
         }
     }
 
-    @NonNull
-    private PointF getPosition(int i, @NonNull Shot shot) {
-        if (selected == shot.getIndex() && selectedPosition != null) {
-            return new PointF(selectedPosition.x, selectedPosition.y);
+    private fun getPosition(i: Int, shot: Shot): PointF {
+        if (selected == shot.index && selectedPosition != null) {
+            return PointF(selectedPosition!!.x, selectedPosition!!.y)
         } else {
-            PointF coordinate = new PointF();
-            float column = i % shotsPerRow + 0.5f;
-            coordinate.x = rect.left + column * columnWidth;
-            float row = (float) (Math.ceil(i / shotsPerRow) + 0.5);
-            coordinate.y = rect.top + row * rowHeight;
-            return coordinate;
+            val coordinate = PointF()
+            val column = i % shotsPerRow + 0.5f
+            coordinate.x = rect!!.left + column * columnWidth
+            val row = (Math.ceil((i / shotsPerRow).toDouble()) + 0.5).toFloat()
+            coordinate.y = rect!!.top + row * rowHeight
+            return coordinate
         }
     }
 
-    @NonNull
-    private PointF getAnimatedPosition(int i, @NonNull Shot shot) {
-        PointF coordinate = getPosition(i, shot);
-        if (currentAnimationProgress != -1 && oldCoordinate[shot.getIndex()] != null) {
-            float oldX = oldCoordinate[shot.getIndex()].x;
-            float oldY = oldCoordinate[shot.getIndex()].y;
-            coordinate.x = oldX + (coordinate.x - oldX) * currentAnimationProgress;
-            coordinate.y = oldY + (coordinate.y - oldY) * currentAnimationProgress;
+    private fun getAnimatedPosition(i: Int, shot: Shot): PointF {
+        val coordinate = getPosition(i, shot)
+        val point = oldCoordinate[shot.index]
+        if (currentAnimationProgress != -1f && point != null) {
+            val oldX = point.x
+            val oldY = point.y
+            coordinate.x = oldX + (coordinate.x - oldX) * currentAnimationProgress
+            coordinate.y = oldY + (coordinate.y - oldY) * currentAnimationProgress
         }
-        return coordinate;
+        return coordinate
     }
 
-    private int getRadius(@NonNull Shot shot) {
-        int rad = radius;
-        int oRad = oldRadius;
-        if (selected == shot.getIndex()) {
-            rad = selectedRadius;
-        } else if (oldSelected == shot.getIndex()) {
-            oRad = oldSelectedRadius;
+    private fun getRadius(shot: Shot): Int {
+        var rad = radius
+        var oRad = oldRadius
+        if (selected == shot.index) {
+            rad = selectedRadius
+        } else if (oldSelected == shot.index) {
+            oRad = oldSelectedRadius
         }
-        if (currentAnimationProgress != -1) {
-            return (int) (oRad + (rad - oRad) * currentAnimationProgress);
+        return if (currentAnimationProgress != -1f) {
+            (oRad + (rad - oRad) * currentAnimationProgress).toInt()
         } else {
-            return rad;
+            rad
         }
     }
 
-    @Nullable
-    public Animator getAnimationToSelection(int selectedShot, PointF c, int radius, @Nullable RectF rect) {
+    fun getAnimationToSelection(selectedShot: Int, c: PointF, radius: Int, rect: RectF?): Animator? {
         if (rect == null) {
-            setSelection(selectedShot, c, radius);
-            return null;
+            setSelection(selectedShot, c, radius)
+            return null
         }
         if (this.rect == null) {
-            setRect(rect);
+            setRect(rect)
         }
-        saveCoordinates();
-        setRect(rect);
-        setSelection(selectedShot, c, radius);
-        return getAnimator();
+        saveCoordinates()
+        setRect(rect)
+        setSelection(selectedShot, c, radius)
+        return animator
     }
 
-    public void setSelection(int selectedShot, PointF c, int radius) {
-        selected = selectedShot;
-        selectedPosition = c;
-        selectedRadius = radius;
+    fun setSelection(selectedShot: Int, c: PointF, radius: Int) {
+        selected = selectedShot
+        selectedPosition = c
+        selectedRadius = radius
     }
 
-    private ValueAnimator getAnimator() {
-        ValueAnimator selectionAnimator = ValueAnimator.ofFloat(0, 1);
-        selectionAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
-        selectionAnimator.addUpdateListener(valueAnimator -> {
-            currentAnimationProgress = (Float) valueAnimator.getAnimatedValue();
-            parent.invalidate();
-        });
-        selectionAnimator.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                currentAnimationProgress = -1;
-            }
-        });
-        return selectionAnimator;
-    }
-
-    private void saveCoordinates() {
-        oldSelectedRadius = selectedRadius;
-        oldRadius = radius;
-        oldSelected = selected;
-        for (int i = 0; i < shotList.size(); i++) {
-            oldCoordinate[shotList.get(i).getIndex()] = getPosition(i, shotList.get(i));
+    private fun saveCoordinates() {
+        oldSelectedRadius = selectedRadius
+        oldRadius = radius
+        oldSelected = selected
+        for (i in shotList!!.indices) {
+            oldCoordinate[shotList!![i].index] = getPosition(i, shotList!![i])
         }
     }
 
-    public int getPressedPosition(float x, float y) {
-        if (rect.contains(x, y)) {
-            int col = (int) Math.floor((x - rect.left) / columnWidth);
-            int row = (int) Math.floor((y - rect.top) / rowHeight);
-            final int arrow = row * shotsPerRow + col;
-            if (arrow < shotList.size() &&
-                    shotList.get(arrow).getScoringRing() != Shot.Companion.getNOTHING_SELECTED()) {
-                return shotList.get(arrow).getIndex() == selected ? -1 :
-                        shotList.get(arrow).getIndex();
+    fun getPressedPosition(x: Float, y: Float): Int {
+        if (rect!!.contains(x, y)) {
+            val col = Math.floor(((x - rect!!.left) / columnWidth).toDouble()).toInt()
+            val row = Math.floor(((y - rect!!.top) / rowHeight).toDouble()).toInt()
+            val arrow = row * shotsPerRow + col
+            if (arrow < shotList!!.size && shotList!![arrow].scoringRing != Shot.NOTHING_SELECTED) {
+                return if (shotList!![arrow].index == selected)
+                    -1
+                else
+                    shotList!![arrow].index
             }
         }
-        return -1;
+        return -1
     }
 
-    public void setPressed(int pressed) {
+    fun setPressed(pressed: Int) {
         if (this.pressed != pressed) {
-            this.pressed = pressed;
-            parent.invalidate();
+            this.pressed = pressed
+            parent!!.invalidate()
         }
     }
 
-    public void setAmbientMode(boolean ambientMode) {
-        this.ambientMode = ambientMode;
+    fun setAmbientMode(ambientMode: Boolean) {
+        this.ambientMode = ambientMode
     }
 
-    @NonNull
-    public Rect getBoundsForShot(int index) {
-        PointF position = getPosition(index, shotList.get(index));
-        float radius = getRadius(shotList.get(index)) * density;
-        return new Rect((int) (position.x - radius), (int) (position.y - radius),
-                (int) (position.x + radius), (int) (position.y + radius));
+    fun getBoundsForShot(index: Int): Rect {
+        val position = getPosition(index, shotList!![index])
+        val radius = getRadius(shotList!![index]) * density
+        return Rect((position.x - radius).toInt(), (position.y - radius).toInt(),
+                (position.x + radius).toInt(), (position.y + radius).toInt())
+    }
+
+    companion object {
+        val NO_SELECTION = -1
+        val MAX_CIRCLE_SIZE = 17
+        private val MIN_PADDING = 2
     }
 }
